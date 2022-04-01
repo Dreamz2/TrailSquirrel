290f9004ee7ed84407af677174bc0f599c5e756a
Kas1680 <kas1914080@gmail.com>
2022-03-31 19:07:38 -0700

    first commit

---
+++ main.test.js
@@ -0,0 +1,25 @@
+
+const m = require('./main.js')
+const w = require('./weather_forecast.js')
+const {queryManager, displayMapFromQuery, displayWeather, initMap} = require('./main')
+
+
+test('does initMap function get invoked at the beginning', () => {
+  // let latitude = 49.2248;
+  // let longitude = -123.1085;
+  // const mockedFn = m.initMap;
+  expect(initMap).toHaveBeenCalled();
+})
+
+test('Expect initMap to provide a default value for long/lat @ Richmond, B.C.', () => {
+    const key = "Enter";
+    m.queryManager(key);
+    expect(displayMapFromQuery).toHaveBeenCalled();
+})
+
+test('Expect displayWeather to be invoked', () => {
+  const key = "Enter";
+  m.displayWeather(key);
+  expect(m.displayMapFromQuery).toHaveBeenCalled();
+})
+
